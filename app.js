7c031b9479a112a53294da8a46db69e02f0e4871
shan5742 <asamshan456@gmail.com>
2020-08-25 21:57:52 +0100

    adding chaulk and nodemon

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const validator = require("validator");
 const getNotes = require("./notes.js");
+const chalk = require("chalk");
 
 const msg = getNotes();
+console.log(chalk.blue.bgRed.bold(msg));
 
-console.log(msg);
+console.log(chalk.green.bold.inverse("Success!"));
