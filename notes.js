f842f08de9c3b4be9748129e0d675debdf46acb2
shan5742 <asamshan456@gmail.com>
2020-08-26 08:54:32 +0100

    removing unused function

--- notes.js
+++ notes.js
@@ -1,10 +1,6 @@
 const fs = require("fs");
 const chalk = require("chalk");
 
-const getNotes = () => {
-  return "Your notes....";
-};
-
 const addNote = (title, body) => {
   const notes = loadNotes();
   const duplicateNote = notes.find((note) => note.title === title);
@@ -79,7 +75,6 @@ const loadNotes = () => {
 };
 
 module.exports = {
-  getNotes: getNotes,
   addNote: addNote,
   removeNote: removeNote,
   listNotes: listNotes,
